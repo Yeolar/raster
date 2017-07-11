a9975b038c3dfab382d5e02c529193ba2a420337
Yeolar <yeolar@gmail.com>
2017-07-11 19:56:53 +0800

    remove old-style ThreadPool which based on pthread, use CPUThreadPool instead.

--- examples/empty/Bench.cpp
+++ examples/empty/Bench.cpp
@@ -3,13 +3,11 @@
  */
 
 #include <gflags/gflags.h>
+#include "rddoc/concurrency/CPUThreadPool.h"
 #include "rddoc/net/NetUtil.h"
 #include "rddoc/protocol/thrift/SyncClient.h"
 #include "rddoc/util/Algorithm.h"
 #include "rddoc/util/Logging.h"
-#include "rddoc/util/Thread.h"
-#include "rddoc/util/ThreadPool.h"
-#include "rddoc/util/Time.h"
 #include "gen-cpp/Empty.h"
 
 static const char* VERSION = "1.0.0";
@@ -22,63 +20,28 @@ DEFINE_int32(count, 100, "request count");
 namespace rdd {
 namespace empty {
 
-class BenchTask : public ThreadTask {
-public:
-  BenchTask(const ClientOption& opt) : ThreadTask(), opt_(opt) {}
-  virtual ~BenchTask() {}
+bool request(const ClientOption& opt) {
+  Query req;
+  req.__set_traceid("rddt");
+  req.__set_query("query");
+  req.__set_forward(FLAGS_forward);
+  Result res;
 
-  virtual void run() {
-    uint64_t t = timestampNow();
-    success_ = request();
-    timecost_ = timePassed(t);
+  TSyncClient<EmptyClient> client(opt);
+  if (!client.connect()) {
+    return false;
   }
-
-  bool request() {
-    Query req;
-    req.__set_traceid("rddt");
-    req.__set_query("query");
-    req.__set_forward(FLAGS_forward);
-    Result res;
-
-    TSyncClient<EmptyClient> client(opt_);
-    if (!client.connect()) {
+  try {
+    client.fetch(&EmptyClient::run, res, req);
+    if (res.code != 0) {
       return false;
     }
-    try {
-      client.fetch(&EmptyClient::run, res, req);
-      if (res.code != 0) {
-        return false;
-      }
-    }
-    catch (...) {
-      return false;
-    }
-    return true;
   }
-
-private:
-  ClientOption opt_;
-};
-
-class BenchThread : public ThreadBase {
-public:
-  typedef ThreadPoolBase<BenchThread> Pool;
-
-  BenchThread(Pool* pool) : pool_(pool) {}
-  virtual ~BenchThread() {}
-
-  virtual void run() {
-    BenchTask* task = nullptr;
-    while ((task = (BenchTask*)pool_->getTask())) {
-      task->run();
-      pool_->finishTask(task);
-      delete task;
-    }
+  catch (...) {
+    return false;
   }
-
-private:
-  Pool* pool_;
-};
+  return true;
+}
 
 }
 }
@@ -90,7 +53,16 @@ int main(int argc, char* argv[]) {
   google::SetUsageMessage("Usage : ./empty-bench");
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  empty::BenchThread::Pool pool(FLAGS_threads);
+  CPUThreadPool pool(FLAGS_threads);
+  std::atomic<size_t> count(0);
+  std::vector<uint64_t> costs(FLAGS_count);
+
+  auto s = pool.subscribeToTaskStats(
+      Observer<Task::Stats>::create(
+          [&](Task::Stats stats) {
+        costs[count++] = stats.runTime;
+      }));
+
   ClientOption opt;
   opt.peer = Peer(FLAGS_addr);
   opt.timeout.ctimeout = 10000000;
@@ -98,28 +70,25 @@ int main(int argc, char* argv[]) {
   opt.timeout.wtimeout = 10000000;
 
   for (int i = 0; i < FLAGS_count; i++) {
-    pool.addTask(new empty::BenchTask(opt));
+    pool.add(std::bind(empty::request, opt));
   }
 
-  pool.start();
-  while (pool.waitingCount() > 0) {
+  while (pool.getStats().pendingTaskCount > 0) {
     sleep(1);
-    RDDRLOG(INFO) << "handled: " << pool.successCount() + pool.failCount();
+    RDDRLOG(INFO) << "handled: " << count;
   }
-  pool.finish();
+  pool.join();
 
   RDDRLOG(INFO) << "FINISH";
-  RDDRLOG(INFO) << "success: " << pool.successCount();
-  RDDRLOG(INFO) << "   fail: " << pool.failCount();
+  RDDRLOG(INFO) << "total: " << count;
 
-  if (pool.successCount() > 0) {
-    auto costs = pool.taskCosts().values();
+  if (count > 0) {
     std::sort(costs.begin(), costs.end());
-    uint64_t cost10 = costs[pool.successCount()    /10];
-    uint64_t cost50 = costs[pool.successCount() * 5/10];
-    uint64_t cost90 = costs[pool.successCount() * 9/10];
+    uint64_t cost10 = costs[count    /10];
+    uint64_t cost50 = costs[count * 5/10];
+    uint64_t cost90 = costs[count * 9/10];
     uint64_t cost_sum = sum(costs);
-    uint64_t cost_avg = cost_sum / pool.successCount();
+    uint64_t cost_avg = cost_sum / count;
 
     RDDRLOG(INFO) << " cost10: " << cost10   / 1000.0 << " ms";
     RDDRLOG(INFO) << " cost50: " << cost50   / 1000.0 << " ms";
