2aee1559d5c807e6c35eda4b135de7fc963d5024
Yeolar <yeolar@gmail.com>
2017-08-08 11:13:06 +0800

    Add no dependency plugin: flume.

--- raster/plugins/flume/FlumeClient.cpp
+++ raster/plugins/flume/FlumeClient.cpp
@@ -4,8 +4,45 @@
 
 #include <fstream>
 #include "raster/plugins/flume/FlumeClient.h"
+#include "raster/plugins/flume/gen-cpp/Scribe.h"
+#include "raster/plugins/flume/gen-cpp/scribe_types.h"
 
 namespace rdd {
+
+FlumeClient::FlumeClient(const ClientOption& option,
+                         const std::string& category,
+                         const std::string& logDir)
+  : category_(category), logDir_(logDir), queue_(10240) {
+  client_ = std::make_shared<TSyncClient<ScribeClient>>(option);
+  if (!client_->connect()) {
+    RDDLOG(ERROR) << "connect flume client failed";
+  }
+}
+
+FlumeClient::~FlumeClient() {
+  send();
+}
+
+bool FlumeClient::add(const std::string& message) {
+  if (queue_.isFull()) {
+    return false;
+  }
+  LogEntry entry;
+  entry.category = category_;
+  entry.message = message + "\n";
+  queue_.write(entry);
+  return true;
+}
+
+void FlumeClient::send() {
+  std::vector<LogEntry> entries;
+  while (!queue_.isEmpty()) {
+    LogEntry entry;
+    queue_.read(entry);
+    entries.push_back(entry);
+  }
+  sendInBatch(entries);
+}
 
 void FlumeClient::sendInBatch(const std::vector<LogEntry>& entries) {
   if (!client_->connected() && !client_->connect()) {
@@ -13,7 +50,7 @@ void FlumeClient::sendInBatch(const std::vector<LogEntry>& entries) {
     writeToDisk(entries);
     return;
   }
-  ResultCode::type r;
+  ResultCode r;
   if (!client_->fetch(&ScribeClient::Log, r, entries) ||
     r != ResultCode::OK) {
     RDDLOG(ERROR) << "write to flume client failed";
--- raster/plugins/flume/FlumeClient.h
+++ raster/plugins/flume/FlumeClient.h
@@ -7,8 +7,6 @@
 #include <functional>
 #include <memory>
 #include <vector>
-#include "raster/plugins/flume/gen-cpp/Scribe.h"
-#include "raster/plugins/flume/gen-cpp/scribe_types.h"
 #include "raster/protocol/thrift/SyncClient.h"
 #include "raster/util/Conv.h"
 #include "raster/util/Logging.h"
@@ -16,42 +14,20 @@
 
 namespace rdd {
 
+class ScribeClient;
+class LogEntry;
+
 class FlumeClient {
 public:
   FlumeClient(const ClientOption& option,
-         const std::string& category,
-         const std::string& logDir = "/opt/logs/logs/flume_delay")
-    : category_(category), logDir_(logDir), queue_(10240) {
-    client_ = std::make_shared<TSyncClient<ScribeClient>>(option);
-    if (!client_->connect()) {
-      RDDLOG(ERROR) << "connect flume client failed";
-    }
-  }
+              const std::string& category,
+              const std::string& logDir = "/opt/logs/logs/flume_delay");
 
-  ~FlumeClient() {
-    send();
-  }
+  ~FlumeClient();
 
-  bool add(const std::string& message) {
-    if (queue_.isFull()) {
-      return false;
-    }
-    LogEntry entry;
-    entry.category = category_;
-    entry.message = message + "\n";
-    queue_.write(entry);
-    return true;
-  }
+  bool add(const std::string& message);
 
-  void send() {
-    std::vector<LogEntry> entries;
-    while (!queue_.isEmpty()) {
-      LogEntry entry;
-      queue_.read(entry);
-      entries.push_back(entry);
-    }
-    sendInBatch(entries);
-  }
+  void send();
 
 private:
   void sendInBatch(const std::vector<LogEntry>& entries);
