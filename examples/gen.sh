9315527a33ee86917c17a9a78a77039767203085
Yeolar <yeolar@gmail.com>
2017-09-28 15:20:15 +0800

    Update travis.

--- examples/gen.sh
+++ examples/gen.sh
@@ -1,16 +1,23 @@
 ROOT=`pwd`
 
 # empty
+echo "Generate files for Empty.thrift"
 cd $ROOT/empty && thrift --gen cpp -out gen-cpp Empty.thrift
 
 # proxy
+echo "Generate files for Proxy.thrift"
 cd $ROOT/proxy && thrift --gen cpp -out gen-cpp Proxy.thrift
 
 # parallel
+echo "Generate files for Parallel.thrift"
 cd $ROOT/parallel && thrift --gen cpp -out gen-cpp Parallel.thrift
 
 # pbrpc
+echo "Generate files for Proxy.proto"
 cd $ROOT/pbrpc && protoc --cpp_out=. Proxy.proto
 
 # flatbuffers
-command -v flatc && cd $ROOT/flatbuffers && flatc --cpp table.fbs
+if [ `command -v flatc` ]; then
+    echo "Generate files for table.fbs"
+    cd $ROOT/flatbuffers && flatc --cpp table.fbs
+fi
