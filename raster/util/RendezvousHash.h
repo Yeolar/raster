966e173429adcfbe32e3d4107d354f87c0da72e2
Yeolar <yeolar@gmail.com>
2017-12-01 14:04:50 +0800

    Add RendezvousHash as a consistent hash solution.

---
+++ raster/util/RendezvousHash.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015, Facebook, Inc.
+ * Copyright (C) 2017, Yeolar
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace rdd {
+
+class RendezvousHash {
+public:
+  /**
+   * build() builds the hashing pool based on a vector of nodes with
+   * their keys and weights.
+   */
+  void build(std::vector<std::pair<std::string, uint64_t>>&);
+
+  /**
+   * get(key, N) finds the node ranked N in the consistent hashing space
+   * for the given key.
+   */
+  size_t get(const uint64_t key, const size_t rank = 0) const;
+
+  /**
+   * get max error rate the current hashing space
+   */
+  double getMaxErrorRate() const;
+
+private:
+  uint64_t computeHash(const char* data, size_t len) const;
+
+  uint64_t computeHash(uint64_t i) const;
+
+  std::vector<std::pair<uint64_t, uint64_t>> weights_;
+};
+
+} // namespace rdd
