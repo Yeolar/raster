934ba8b819c7c5f0decea828eb965c292c1d213d
Yeolar <yeolar@gmail.com>
2017-12-26 21:17:32 +0800

    refactoring on enc.

--- raster/enc/CharProperty.cpp
+++ raster/enc/CharProperty.cpp
@@ -11,13 +11,17 @@
 
 namespace rdd {
 
-#define MAX_UNICODE_LENGTH  65536
-#define MAX_CATEGORY_STRLEN 32
+static constexpr size_t kMaxUnicodeLength = 65536;
+static constexpr size_t kMaxCategoryLength = 32;
 
-uint16_t gUnicodeToGBKTable[MAX_UNICODE_LENGTH] = {
+uint16_t gUnicodeToGBKTable[kMaxUnicodeLength] = {
 #include "un2gbkmap.h"
 };
 
+/*
+ * format:
+ *  | n (uint32) | category ... (char[32] * n) | charinfo ... (charinfo * n) |
+ */
 bool CharProperty::open(const std::string& filename) {
   if (!readFile(filename.c_str(), data_)) {
     RDDLOG(ERROR) << "read char property file " << filename << " failed";
@@ -25,18 +29,43 @@ bool CharProperty::open(const std::string& filename) {
   }
   uint32_t n = *((uint32_t*)data_.data());
   uint32_t offset = sizeof(uint32_t);
-  if (data_.length() != (offset + MAX_CATEGORY_STRLEN * n +
-                         sizeof(uint32_t) * 0xffff)) {
+  if (data_.size() != (offset + kMaxCategoryLength * n
+                       + sizeof(uint32_t) * 0xffff)) {
     RDDLOG(ERROR) << "illegal size of char property file " << filename;
     return false;
   }
   categories_.clear();
   for (uint32_t i = 0; i < n; ++i) {
     categories_.push_back(&data_[offset]);
-    offset += MAX_CATEGORY_STRLEN;
+    offset += kMaxCategoryLength;
   }
   map_ = (const CharInfo*) &data_[offset];
   return true;
+}
+
+size_t CharProperty::size() const {
+  return categories_.size();
+}
+
+std::string CharProperty::name(size_t i) const {
+  return categories_[i].str();
+}
+
+size_t CharProperty::id(StringPiece key) const {
+  for (size_t i = 0; i < categories_.size(); ++i) {
+    if (key == categories_[i]) {
+      return i;
+    }
+  }
+  return std::numeric_limits<size_t>::max();
+}
+
+CharInfo CharProperty::getCharInfo(char16_t cp) const {
+  return map_[cp];
+}
+
+CharInfo CharProperty::getCharInfo(StringPiece key) const {
+  return CharInfo(id(key));
 }
 
 char16_t readHex(StringPiece s) {
@@ -47,9 +76,9 @@ char16_t readHex(StringPiece s) {
            -1;
   };
   // 0x0000 - 0xffff
-  char16_t r = unhex(s[2]) * 4096
-             + unhex(s[3]) * 256
-             + unhex(s[4]) * 16
+  char16_t r = unhex(s[2]) * 0x1000
+             + unhex(s[3]) * 0x100
+             + unhex(s[4]) * 0x10
              + unhex(s[5]);
   return r;
 }
@@ -90,7 +119,7 @@ CharInfo encode(const std::vector<StringPiece>& c,
       RDDLOG(ERROR) << "category[" << i << "] is undefined";
       return base;
     }
-    base.type += it->second.type;
+    base += it->second;
   }
   return base;
 }
@@ -116,7 +145,7 @@ bool CharProperty::compile(const std::string& cfile,
       continue;
     if (line.subpiece(0, 2) != "0x") {
       auto c = trimWhitespace(line);
-      if (c.size() >= MAX_CATEGORY_STRLEN) {
+      if (c.size() >= kMaxCategoryLength) {
         RDDLOG(ERROR) << "category[" << c << "] too long";
         return false;
       }
@@ -138,7 +167,7 @@ bool CharProperty::compile(const std::string& cfile,
   for (auto& range : ranges) {
     CharInfo ci = encode(range.c, cmap);
     for (char16_t c = range.l; c <= range.h; ++c) {
-      table[c] = table[c] | ci;
+      table[c] |= ci;
     }
   }
   if (charset == "gbk") {
@@ -161,13 +190,32 @@ bool CharProperty::compile(const std::string& cfile,
   out.append((char*)&n, sizeof(n));
   for (auto& c : carray) {
     out.append(c.str());
-    out.append(MAX_CATEGORY_STRLEN - c.size(), '\0');
+    out.append(kMaxCategoryLength - c.size(), '\0');
   }
   out.append((char*)&table[0], sizeof(CharInfo) * table.size());
   if (!writeFile(out, ofile.c_str(), 0644)) {
     RDDLOG(ERROR) << "write char property file " << ofile << " failed";
     return false;
   }
+  return true;
+}
+
+bool PeckerCharProperty::open(const std::string& filename) {
+  if (!CharProperty::open(filename)) {
+    return false;
+  }
+  space_      = getCharInfo("SPACE");
+  alphaLower_ = getCharInfo("ALPHA_LOWER");
+  alphaUpper_ = getCharInfo("ALPHA_UPPER");
+  asciiDigit_ = getCharInfo("ASCII_DIGIT");
+  asciiPunc_  = getCharInfo("ASCII_PUNC");
+  hanziChar_  = getCharInfo("HANZI_CHAR");
+  hanziDigit_ = getCharInfo("HANZI_DIGIT");
+  hanziTime_  = getCharInfo("HANZI_TIME");
+  fullWidth_  = getCharInfo("FULL_WIDTH");
+  stopPunc_   = getCharInfo("STOP_PUNC");
+  alpha_      = alphaLower_ | alphaUpper_;
+  alnum_      = alpha_ | asciiDigit_;
   return true;
 }
 
