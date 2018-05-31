50a84970b69727f1421fcdd48c0949a2e35a3192
Yeolar <yeolar@gmail.com>
2018-05-31 16:58:59 +0800

    update cmake.

--- cmake-scripts/FindFlatbuffers.cmake
+++ cmake-scripts/FindFlatbuffers.cmake
@@ -1,5 +1,6 @@
 # Find flatbuffers
 #
+#  FLATBUFFERS_FLATC       - flatc.
 #  FLATBUFFERS_INCLUDE_DIR - where to find flatbuffers/*.
 #  FLATBUFFERS_LIBRARY     - List of libraries when using flatbuffers.
 #  FLATBUFFERS_FOUND       - True if flatbuffers found.
@@ -12,12 +13,13 @@ if(FLATBUFFERS_INCLUDE_DIR)
   set(FLATBUFFERS_FIND_QUIETLY TRUE)
 endif()
 
+find_program(FLATBUFFERS_FLATC flatc PATHS ${PROJECT_BINARY_DIR}/flatbuffers/flatbuffers/bin)
 find_path(FLATBUFFERS_INCLUDE_DIR flatbuffers/flatbuffers.h PATHS ${PROJECT_BINARY_DIR}/flatbuffers/flatbuffers/include)
 find_library(FLATBUFFERS_LIBRARY flatbuffers PATHS ${PROJECT_BINARY_DIR}/flatbuffers/flatbuffers/lib)
 
 # handle the QUIETLY and REQUIRED arguments and set FLATBUFFERS_FOUND to TRUE 
 # if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FLATBUFFERS DEFAULT_MSG FLATBUFFERS_LIBRARY FLATBUFFERS_INCLUDE_DIR)
+find_package_handle_standard_args(FLATBUFFERS DEFAULT_MSG FLATBUFFERS_LIBRARY FLATBUFFERS_INCLUDE_DIR FLATBUFFERS_FLATC)
 
-mark_as_advanced(FLATBUFFERS_LIBRARY FLATBUFFERS_INCLUDE_DIR)
+mark_as_advanced(FLATBUFFERS_LIBRARY FLATBUFFERS_INCLUDE_DIR FLATBUFFERS_FLATC)
