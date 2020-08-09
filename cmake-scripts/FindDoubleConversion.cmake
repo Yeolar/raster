7b224bb25bb65ec97368c046427a8e6812216af2
Yeolar <yeolar@gmail.com>
2020-08-09 18:11:50 +0800

    update cmake

---
+++ cmake-scripts/FindDoubleConversion.cmake
@@ -0,0 +1,26 @@
+# Finds libdouble-conversion.
+#
+# This module defines:
+# DOUBLE_CONVERSION_INCLUDE_DIR
+# DOUBLE_CONVERSION_LIBRARY
+#
+
+if(DOUBLE_CONVERSION_INCLUDE_DIR)
+  # Already in cache, be silent
+  set(DOUBLE_CONVERSION_FIND_QUIETLY TRUE)
+endif()
+
+find_path(DOUBLE_CONVERSION_INCLUDE_DIR
+    NAMES
+      double-conversion.h
+    PATHS
+      /usr/include/double-conversion
+      /usr/local/include/double-conversion)
+find_library(DOUBLE_CONVERSION_LIBRARY NAMES double-conversion)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    DOUBLE_CONVERSION DEFAULT_MSG
+    DOUBLE_CONVERSION_LIBRARY DOUBLE_CONVERSION_INCLUDE_DIR)
+
+mark_as_advanced(DOUBLE_CONVERSION_INCLUDE_DIR DOUBLE_CONVERSION_LIBRARY)
